22bc5d5cc173d8adc7dbe5beba35c8115da1e1b9
flatironschool <t.benson@live.co.uk>
2020-04-27 13:20:56 +0100

    updated copyright

--- src/SignUp.js
+++ src/SignUp.js
@@ -19,7 +19,7 @@ function Copyright() {
         <Typography variant="body2" color="textSecondary" align="center">
         {"Copyright © "}
         <Link color="inherit" href="https://material-ui.com/">
-            Your Website
+            Tolani Benson
         </Link>{" "}
         {new Date().getFullYear()}
         {"."}
@@ -44,6 +44,8 @@ function Copyright() {
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
     },
     }));
 
